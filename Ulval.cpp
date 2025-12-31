f3a0e677cd00e62f3ae867bb0fc071a5af02ae25
Anay <debanay1@gmail.com>
2025-12-31 11:27:27 +0530

    Logic inside it

--- Ulval.cpp
+++ Ulval.cpp
@@ -1,25 +1,35 @@
+/*
+ * -> Anay Deb
+ * "Everything is EASY... Just needs a little time!" 
+ *  Lets Fucking GOO!!
+ */
+
 #include <bits/stdc++.h>
 using namespace std;
 
 #define fast ios::sync_with_stdio(false); cin.tie(nullptr);
-
 #define int long long
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
 #define pb push_back
+#define pi pair<int,int>
 #define vi vector<int>
 #define vii vector<pair<int,int>>
-#define rep(i,a,b) for(int i = (a); i < (b); i++)
+#define rep(i,a,b) for(int i = (a); i < (b); ++i)
+#define per(i,a,b) for(int i= (b)-1; i>=(a); --i)
+#define endl '\n'
 
-// ===== GCD =====
+// ---- GCD -----
 int gcd(int a, int b) {
     return (b == 0 ? a : gcd(b, a % b));
 }
 
-// ===== LCM =====
+// ----- LCM -----
 int lcm(int a, int b) {
     return (a / gcd(a, b)) * b;
 }
 
-// ===== PRIME CHECK =====
+// ------ PRIME CHECK -----
 bool isPrime(int n) {
     if(n < 2) return false;
     if(n % 2 == 0) return n == 2;
@@ -28,20 +38,53 @@ bool isPrime(int n) {
     return true;
 }
 
-// ===== SOLVE FUNCTION =====
+// ------- SOLVE FUNCTION ------
+/*
+ * Main Logic:
+ * ->	These is a 1500 rated question and the main observation in these question is
+ * 	  	that the game will only continue max to max one round that also , the first
+ * 	  	round of Alice and the most optimal situation for Bob is to just end the game.
+ * 
+ * -> 	So Alice will either finish the game in first round or will make the sum more.
+ * 		But Bob will always end the game.
+ * 
+ * -> 	So , we need to find the 2 indices that alice will swap so that the sum eventually
+ * 		increases more and gets more sum.
+ * 
+ * -> 	Now the question is how many ways alice can chose the indices 
+ * 		-> There are basically 4 ways to choose :-
+ * 			-> first he can chose + +
+ * 			-> second he can chose - -
+ * 			-> third he can chose - +
+ * 			-> fourth he can chose + -
+ * 
+ * -> 	Same same cases can handled just by if( n is even ) then cost+ n-2;
+ * 	  	else cost + n-1;
+ * 
+ * -> 	For the alternate sign cases we need to variable to keep the maxi and mini
+ * 		and also the maximum sum that we can get.
+ * 
+ * 
+ * -> 	Hence thats the code and thats the whole logic !
+ 
+ */
 void solve() {
 	int n; cin>>n;
 	
 	vi a(n);
-	rep(i,0,n) cin>>a[i];
+	per(i,0,n) cin>>a[i];
+	reverse(all(a));
 	
 	int sum=0;
 	rep(i,0,n){
-		if(i & 1) sum-=a[i];
+		if(i & 1){
+			sum-=a[i];
+		}
 		else sum+=a[i];
 	}
-	int val=0;
+	
 	int maxi=-1e18,mini=1e18;
+	int val=0;
 	rep(i,0,n){
 		if(i & 1){
 			maxi=max(maxi,2*a[i]-i);
@@ -53,8 +96,7 @@ void solve() {
 		}
 	}
 	
-	//cout<<val<<endl;
-	int ans=max({sum,sum+val,sum+((n&1)?n-1:n-2)});
+	int ans=max({sum,sum+val,sum+((n%2==0)? n-2: n-1)});
 	cout<<ans<<endl;
 }
 
