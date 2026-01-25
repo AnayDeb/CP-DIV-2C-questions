9e89f63708dd3b00f18fe310633da733abadb792
Anay <debanay1@gmail.com>
2026-01-25 10:25:34 +0530

    Very beautiful question it needs bfs

---
+++ Dtrees.cpp
@@ -0,0 +1,159 @@
+/*
+ * -> Anay Deb
+ * -> "Everything is EASY... just needs a little time!"
+ * -> Lets Fucking GOO!!
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// ---------- DEBUG TEMPLATE ----------
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(long long x) {cerr << x;}
+void _print(int x) {cerr << x;}
+void _print(double x) {cerr << x;}
+void _print(string x) {cerr << '"' << x << '"';}
+void _print(char x) {cerr << '\'' << x << '\'';}
+void _print(bool x) {cerr << (x ? "true" : "false");}
+
+template<class T, class V> void _print(const pair<T, V> &p);
+template<class T> void _print(const vector<T> &v);
+template<class T> void _print(const set<T> &v);
+template<class T, class V> void _print(const map<T, V> &v);
+template<class T, class V> void _print(const unordered_map<T, V> &v);
+template<class T> void _print(stack<T> s);
+template<class T> void _print(queue<T> q);
+
+template<class T, class V> void _print(const pair<T, V> &p) {
+    cerr << "{"; _print(p.first); cerr << ", "; _print(p.second); cerr << "}";
+}
+template<class T> void _print(const vector<T> &v) {
+    cerr << "[ "; for (const auto &i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template<class T> void _print(const set<T> &v) {
+    cerr << "{ "; for (const auto &i : v) {_print(i); cerr << " ";} cerr << "}";
+}
+template<class T, class V> void _print(const map<T, V> &v) {
+    cerr << "[ "; for (const auto &i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template<class T, class V> void _print(const unordered_map<T, V> &v) {
+    cerr << "[ "; for (const auto &i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template<class T> void _print(stack<T> s) {
+    vector<T> res; while (!s.empty()) { res.push_back(s.top()); s.pop(); }
+    reverse(res.begin(), res.end()); cerr << "top->"; _print(res);
+}
+template<class T> void _print(queue<T> q) {
+    vector<T> res; while (!q.empty()) { res.push_back(q.front()); q.pop(); }
+    cerr << "front->"; _print(res);
+}
+// ---------- END DEBUG TEMPLATE ----------
+
+
+using ll=long long;
+using pi=pair<int,int>;
+using vi=vector<int>;
+using vii=vector<pair<int,int>>;
+using vil=vector<ll>;
+using pil=pair<ll,ll>;
+using viil=vector<pair<ll,ll>>;
+
+#define fast ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define rep(i,a,b) for(int i = (a); i <= (b); ++i)
+#define per(i,a,b) for(int i = (a); i >= (b); --i)
+#define pb push_back
+#define Ceil(a,b) ((a+b-1)/b)
+#define endl '\n'
+
+
+// ---- GCD -----
+int gcd(int a, int b) {
+    return (b == 0 ? a : gcd(b, a % b));
+}
+
+// ----- LCM -----
+int lcm(int a, int b) {
+    return (a / gcd(a, b)) * b;
+}
+
+// ------ PRIME CHECK -----
+bool isPrime(int n) {
+    if(n < 2) return false;
+    if(n % 2 == 0) return n == 2;
+    for(int i = 3; i * i <= n; i += 2)
+        if(n % i == 0) return false;
+    return true;
+}
+// ------- SOLVE FUNCTION ------
+/*
+    Observations:-
+    -> Given n-1 edges to form a tree.There is a algorithm to draw
+    the tree:
+            -> Draw the first vertex.
+            -> If the edges has already a drawn vertex then he draws
+            it and goes to the next step.
+            -> If all the edges are drawn then terminate else, go to
+            the step 2 again.
+
+    -> Now the question is How many times he need to go to the step2
+    to draw the tree?
+
+    -> node 1 is the root of the tree.
+*/
+void solve() {
+    int n;
+    cin>>n;
+
+    vector<vector<pi>>adj(n+1,vector<pi>());
+    rep(i,1,n-1){
+        int u,v;
+        cin>>u>>v;
+
+        adj[u].pb({v,i});
+        adj[v].pb({u,i});
+    }
+
+    map<int,bool>vis;
+    vis[1]=true;
+    queue<pi>q;
+    q.push({1,0});
+    vi dp(n+1,-1);
+    dp[1]=1;
+    while(!q.empty()){
+        auto [node,incom]=q.front();
+        q.pop();
+
+        for(auto [child,com]:adj[node]){
+            if(vis[child]) continue;
+            if(incom<=com){
+                dp[child]=dp[node];
+            }
+            else{
+                dp[child]=dp[node]+1;
+            }
+            q.push({child,com});
+            vis[child]=true;
+        }
+    }
+    debug(dp);
+
+    cout<<*max_element(all(dp))<<endl;
+}
+
+int32_t main() {
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
